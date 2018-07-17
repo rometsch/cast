80d9a791f0f12886ad2d23d43253acdacbfe5589
Thomas Rometsch <thomas.rometsch@gmail.com>
2018-07-17 10:32:26 +0200

    setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(  name="Common Astrophysics Simulation Tools"
+        ,version="0.1"
+        ,description="Tools for processing simulation output from different codes."
+        ,author="Thomas Rometsch"
+		,packages=['cast']
+        )
